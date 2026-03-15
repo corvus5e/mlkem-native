7570d26fe1583271eacf59f9fabc706ace219593
corvus5e <corvus5e@gmail.com>
2026-03-15 19:29:03 +0100

    Comment on building for x86

--- build_shared_lib.cmd
+++ build_shared_lib.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal
-
+REM  cmake -G "Visual Studio 17 2022" -A Win32 -B build32
 if not exist build mkdir build
 cd build
 cmake -S .. -B .
-cmake --build .
+cmake --build . --config Release
 cd ..
